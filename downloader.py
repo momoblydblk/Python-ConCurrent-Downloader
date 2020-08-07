256ab5458e2f79dc645165c5ecf60f3d5df84ceb
Momoblydblk <momoblydblk@gmail.com>
2020-08-07 14:06:21 +0800

    Sync commit

--- downloader.py
+++ downloader.py
@@ -8,11 +8,11 @@ requests.packages.urllib3.disable_warnings();
 #Set Max_Worker Limit
 max_worker = 32;
 
-#Create downloader Object for each download process.
+#Create downloader Object for Each Download
 class downloader:
     url='';
     fileSize='';
 
     def __init__(self,url):
         self.url = url;  
-        self.fileSize = int(requests.get())
+        self.fileSize = int(requests.get(self.url,stream=True,verify=False).headers['Content-Length'])
